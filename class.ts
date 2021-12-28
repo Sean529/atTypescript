a45e81d56c6a27d483f1b2c35121e04dd70183e6
AT <atxiesz@gmail.com>
2021-12-28 21:18:49 +0800

    feat class

--- class.ts
+++ class.ts
@@ -12,8 +12,8 @@ const p1 = new Person();
 p1.name = 'at'
 p1.getName()
 
+// ________________________________________________________________
 // 定义存取器 ts 通过存取器来改变类中属性的读取和赋值操作
-
 class User {
 	myName: string
 	myAge: number
@@ -43,3 +43,38 @@ user.name = 'AT'
 console.log(user.name);
 console.log(user.age);
 
+
+// ________________________________________________________________
+// 参数属性
+// public
+class User2 {
+	// 公开参数
+	// myName: string
+	constructor(public myName: string) {
+		// this.myName = myName
+	}
+
+	get name() {
+		return this.myName
+	}
+	set name(value: string) {
+		this.myName = value
+	}
+}
+
+const user2 = new User('at', 18)
+user2.name = 'AT'
+console.log(user2.name);
+console.log(user2.age);
+
+// readonly
+class Animal {
+	public readonly name:string
+	constructor(name: string) {
+		this.name = name
+	}
+
+	changeName(name: string) {
+		// this.name = name // 只读属性不可更改
+	}
+}
