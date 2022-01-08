72eff52f069c3f8732f2f2bdeb852993bd095c43
AT <atxiesz@gmail.com>
2022-01-08 20:46:52 +0800

    feat 类型扩展

---
+++ src/typeExpansion.ts
@@ -0,0 +1,31 @@
+export { }
+// 类型扩展
+// ________________________________________________________________
+// 扩展局部变量的类型
+declare var String: StringConstructor
+interface StringConstructor {
+	new(value?: any): String
+	(value?: any): string
+	readonly prototype: String
+}
+interface String {
+	toString(): string
+}
+String.prototype.double = function (): string {
+	return this + this
+}
+
+const result = new String("hello").double() // hellohello
+
+console.log('%c AT-[ result ]-19', 'font-size:13px; background:#de4307; color:#f6d04d;', result)
+interface String {
+	double(): string
+}
+
+// 在局部模块中扩展全局变量使用 declare global
+declare global {
+	interface Window {
+		myName: string
+	}
+}
+console.log(window.myName);
